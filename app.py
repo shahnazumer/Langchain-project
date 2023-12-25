ff146237e53a5150387b8abb6594cf0fee40dd68
shahnazumer <shahnazumer@hotmail.com>
2023-12-26 00:56:21 +0500

    final changes

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
 # Q&A Chatbot
 
 from langchain.llms import OpenAI
-
 from dotenv import load_dotenv
-
-load_dotenv() # take environment variables from .env.
-
-
 import streamlit as st
 import os
 
-## Function to load OpenAI model and get response
+load_dotenv()  # take environment variables from .env.
 
+# Function to load OpenAI model and get response
 def get_openai_response(question):
-    llm=OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"),model_name="text-davinci-003",temperature=0.5)
-    response=llm(question)
+    # Pass the openai_api_key as a named parameter or set it as an environment variable
+    llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), model_name="text-davinci-003", temperature=0.5)
+    response = llm(question)
     return response
 
-##initialize our streamlit app
-
+# Initialize our Streamlit app
 st.set_page_config(page_title="Q&A")
-
 st.header("Langchain Application")
 
-input=st.text_input("Input: ",key="input")
-response=get_openai_response(input)
+# Get user input
+input = st.text_input("Input: ", key="input")
 
-submit=st.button("Ask the question")
+# Get OpenAI response when the "Ask the question" button is clicked
+submit = st.button("Ask the question")
 
-
-## if ask button is clicked
-
+# If ask button is clicked
 if submit:
-    st.subheader("The Response is")
-    st.write(response)
+    # Ensure there is input before getting a response
+    if input:
+        st.subheader("The Response is")
+        response = get_openai_response(input)
+        st.write(response)
+    else:
+        st.warning("Please enter a question before clicking the 'Ask the question' button.")
