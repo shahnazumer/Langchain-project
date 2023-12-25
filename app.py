83829b07beed89f5300c15c97082f8c6ece92f25
shahnazumer <shahnazumer@hotmail.com>
2023-12-26 00:26:41 +0500

    Final Changes

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv() # take environment variables from .env.
 
+
 import streamlit as st
 import os
 
 ## Function to load OpenAI model and get response
 
 def get_openai_response(question):
-    llm=OpenAI(openai_api_key=os.getenv("OPEN_API_KEY"),model_name="text-davinci-003",temperature=0.5)
+    llm=OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"),model_name="text-davinci-003",temperature=0.5)
     response=llm(question)
     return response
 
